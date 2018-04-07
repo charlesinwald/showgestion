43c5e173245eaaf686f18f52dd01d8c569aabcb3
Charles Inwald <cci219@lehigh.edu>
2018-04-07 12:42:38 -0400

    Automated deploy

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/magic`', function (req, res) {
+app.get('/magic', function (req, res) {
     var request = require("request");
 
     var options = { method: 'POST',

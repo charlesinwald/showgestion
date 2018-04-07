841a34e57bb8f071309e67fefc387b82858a157b
Charles Inwald <cci219@lehigh.edu>
2018-04-07 12:39:35 -0400

    Automated deploy

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', function (req, res) {
+app.get('/magic`', function (req, res) {
     var settings = {
         "async": true,
         "crossDomain": true,

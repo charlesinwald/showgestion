9ec40bb274c49a0cb4183fc65a8d709b994c7f0f
Charles Inwald <cci219@lehigh.edu>
2018-04-07 12:38:54 -0400

    Automated deploy

--- app.js
+++ app.js
@@ -18,6 +18,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function (req, res) {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.applymagicsauce.com/like_ids?interpretations=true",
+        "method": "POST",
+        "headers": {
+            "X-Auth-Token": "7nbe03d055jq8pp175b6amgsaa",
+            "Content-type": "application/json",
+            "Accept": "application/json",
+            "Cache-Control": "no-cache",
+            "Postman-Token": "c84f786f-4009-4aa3-b9ac-d47e05efab98"
+        },
+        "data": "[\"302383559805193\", \"6049803276\", \"18510635068\", \"47923519017\", \"35649991492\", \"185019191542371\", \"313227042137369\", \"878737018959036\", \"523598997680162\", \"199617896754080\", \"117533210756\", \"112584912087017\", \"98102468683\"]"
+    }
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        res.send(response);
+    });
+})
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
